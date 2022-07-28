aaa60edb2c585f8dce73a0df84aa9d5277d24510
Ali Alirahimi <alialirahimi642@gmail.com>
2022-07-28 20:42:35 +0430

    for each and other properties for Widgets
    Layout Learning - End project

--- cv_app/lib/main.dart
+++ cv_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(Application());
@@ -36,23 +37,29 @@ class Application extends StatelessWidget {
   }
 
   Widget _getBody() {
-    return Column(
-      children: [
-        _getHeader(),
-      ],
+    return Center(
+      child: Column(
+        children: [
+          _getHeader(),
+          _getIconsRow(),
+          _getSkillLabels(),
+          SizedBox(
+            height: 12.0,
+          ),
+          _getResume(),
+        ],
+      ),
     );
   }
 
   Widget _getHeader() {
     return Column(
       children: [
-        SizedBox(height: 30),
-        Center(
-          child: CircleAvatar(
-            backgroundColor: Colors.brown,
-            backgroundImage: AssetImage('images/flutter.png'),
-            radius: 80,
-          ),
+        SizedBox(height: 20),
+        CircleAvatar(
+          backgroundColor: Colors.brown,
+          backgroundImage: AssetImage('images/flutter1.png'),
+          radius: 50,
         ),
         SizedBox(height: 20),
         Text(
@@ -63,4 +70,143 @@ class Application extends StatelessWidget {
       ],
     );
   }
+
+  Widget _getIconsRow() {
+    return Wrap(
+      runSpacing: 20.0,
+      spacing: 30.0,
+      alignment: WrapAlignment.center,
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.instagram),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.telegram),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.github),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: FaIcon(FontAwesomeIcons.linkedin),
+        ),
+      ],
+    );
+  }
+
+  Widget _getSkillLabels() {
+    var names = ['flutter', 'git', 'kotlin', 'wordpress', 'java', 'android'];
+    var images = [
+      'flutter1',
+      'git1',
+      'kotlin1',
+      'wordpress1',
+      'java1',
+      'android1'
+    ];
+    return Wrap(
+      alignment: WrapAlignment.center,
+      runSpacing: 5.0,
+      spacing: 5.0,
+      children: [
+        //for(var names in names)
+        for (int index = 0; index < names.length; index++)
+          Card(
+            elevation: 15,
+            shadowColor: Colors.blue,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(7),
+            ),
+            child: Column(
+              children: [
+                Container(
+                  width: 30.0,
+                  height: 30.0,
+                  child: Image(
+                    image: AssetImage('images/${images[index]}.png'),
+                  ),
+                ),
+                Padding(
+                  child: Text(names[index]),
+                  padding: EdgeInsets.all(5),
+                ),
+              ],
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget _getResume() {
+    var resumeContext = ['متن 1', 'متن 2', 'متن 3', 'متن 4', 'متن 5'];
+    return Column(
+      children: [
+        Container(
+          color: Colors.blue[400],
+          width: double.infinity,
+          child: Text(
+            'سابقه کاری',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Container(
+          color: Colors.grey[400],
+          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(
+                width: double.infinity,
+              ),
+              for (var index in resumeContext)
+                Text(
+                  index,
+                  textDirection: TextDirection.rtl,
+                ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
 }
+  /* Wrap(
+      spacing: 15,
+      alignment: WrapAlignment.center,
+      children: [
+        getFlutterCard(),
+        getFlutterCard(),
+        getFlutterCard(),
+        getFlutterCard(),
+      ],
+    ); */
+
+  /* Widget getFlutterCard() {
+    return Card(
+      elevation: 15,
+      shadowColor: Colors.blue,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: 60.0,
+            height: 60.0,
+            child: Image(
+              image: AssetImage('images/flutter.png'),
+            ),
+          ),
+          Padding(
+            child: Text('flutter'),
+            padding: EdgeInsets.all(5),
+          ),
+        ],
+      ),
+    );
+  } */
+
