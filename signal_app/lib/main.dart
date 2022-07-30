3936119352695fd6aa57ec8710fd9830bd67e1a1
Ali Alirahimi <alialirahimi642@gmail.com>
2022-07-30 15:37:59 +0430

    Create Login Screen

--- signal_app/lib/main.dart
+++ signal_app/lib/main.dart
@@ -10,6 +10,7 @@ class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(fontFamily: 'vazir'),
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
@@ -22,21 +23,64 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber,
       appBar: _getHomeScreenBar(),
-      body: SafeArea(
-        child: Center(
-          child: TextButton(
-            child: Text('Home Page'),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (BuildContext context) => SecondScreen(),
-                ),
-              );
-            },
-          ),
+      body: _getBody(context),
+    );
+  }
+
+  Widget _getBody(BuildContext context) {
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(width: double.infinity, height: 40.0),
+            Text(
+              'خوش آمدید!',
+              textDirection: TextDirection.rtl,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+            ),
+            SizedBox(height: 10.0),
+            Image(
+              image: AssetImage('images/welcome.png'),
+              height: 300.0,
+            ),
+            _getSignUpButton(),
+            _getSignInButton(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _getSignUpButton() {
+    return OutlinedButton(
+      style: OutlinedButton.styleFrom(
+          minimumSize: Size(200.0, 40.0),
+          side: BorderSide(color: Colors.black, width: 2.0),
+          primary: Colors.black),
+      onPressed: () {},
+      child: Text(
+        'ثبت نام',
+        style: TextStyle(fontSize: 16.0),
+      ),
+    );
+  }
+
+  Widget _getSignInButton() {
+    return TextButton(
+      style: TextButton.styleFrom(
+        minimumSize: Size(200.0, 40.0),
+        primary: Colors.black,
+        backgroundColor: Colors.white,
+      ),
+      child: Text(
+        'ورود',
+        style: TextStyle(fontSize: 16.0),
+      ),
+      onPressed: () {},
     );
   }
 
@@ -57,20 +101,24 @@ class SecondScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _getSecondScreenBar(),
-      body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () {
-              Navigator.of(context).pop(
-                MaterialPageRoute(
-                  builder: (BuildContext context) {
-                    return HomeScreen();
-                  },
-                ),
-              );
-            },
-            child: Text('Go to first page'),
-          ),
+      body: _getBody(context),
+    );
+  }
+
+  Widget _getBody(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop(
+              MaterialPageRoute(
+                builder: (BuildContext context) {
+                  return HomeScreen();
+                },
+              ),
+            );
+          },
+          child: Text('Go to first page'),
         ),
       ),
     );
