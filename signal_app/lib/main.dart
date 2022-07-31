7cc318418205e3872fab0f6d4744453bb6d50af2
Ali Alirahimi <alialirahimi642@gmail.com>
2022-07-31 18:40:14 +0430

    Icon and Elevated Button
    Elevated Button Widget and Creates Icons + Separated Classes

--- signal_app/lib/main.dart
+++ signal_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'login_page.dart';
 
 void main() {
   runApp(Application());
@@ -12,84 +13,7 @@ class Application extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'vazir'),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.amber,
-      appBar: _getHomeScreenBar(),
-      body: _getBody(context),
-    );
-  }
-
-  Widget _getBody(BuildContext context) {
-    return SafeArea(
-      child: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            SizedBox(width: double.infinity, height: 40.0),
-            Text(
-              'خوش آمدید!',
-              textDirection: TextDirection.rtl,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
-            ),
-            SizedBox(height: 10.0),
-            Image(
-              image: AssetImage('images/welcome.png'),
-              height: 300.0,
-            ),
-            _getSignUpButton(),
-            _getSignInButton(),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _getSignUpButton() {
-    return OutlinedButton(
-      style: OutlinedButton.styleFrom(
-          minimumSize: Size(200.0, 40.0),
-          side: BorderSide(color: Colors.black, width: 2.0),
-          primary: Colors.black),
-      onPressed: () {},
-      child: Text(
-        'ثبت نام',
-        style: TextStyle(fontSize: 16.0),
-      ),
-    );
-  }
-
-  Widget _getSignInButton() {
-    return TextButton(
-      style: TextButton.styleFrom(
-        minimumSize: Size(200.0, 40.0),
-        primary: Colors.black,
-        backgroundColor: Colors.white,
-      ),
-      child: Text(
-        'ورود',
-        style: TextStyle(fontSize: 16.0),
-      ),
-      onPressed: () {},
-    );
-  }
-
-  PreferredSizeWidget _getHomeScreenBar() {
-    return AppBar(
-      elevation: 10.0,
-      title: Text(
-        'Home Screen',
-      ),
+      home: LoginPage(),
     );
   }
 }
@@ -113,7 +37,7 @@ class SecondScreen extends StatelessWidget {
             Navigator.of(context).pop(
               MaterialPageRoute(
                 builder: (BuildContext context) {
-                  return HomeScreen();
+                  return LoginPage();
                 },
               ),
             );
