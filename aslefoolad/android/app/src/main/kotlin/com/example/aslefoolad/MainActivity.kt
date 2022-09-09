40dab010b65531a1064a368e31d65bf726b24488
Ali Alirahimi <alialirahimi642@gmail.com>
2022-09-09 21:12:42 +0430

    starting aslefoolad project
    learning and implement slider with package

---
+++ aslefoolad/android/app/src/main/kotlin/com/example/aslefoolad/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aslefoolad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aslefoolad/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:aslefoolad/pages/pages.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
