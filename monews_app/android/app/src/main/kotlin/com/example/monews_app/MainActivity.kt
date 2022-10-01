37df7dcc4de2777c4be26d874c1983158afb908d
Ali Alirahimi <alialirahimi642@gmail.com>
2022-10-01 10:56:45 +0330

    Revert "Merge branch 'master'

--- monews_app/android/app/src/main/kotlin/com/example/monews_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.monews_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- monews_app/lib/pages/pages.dart
+++
@@ -1,7 +0,0 @@
-export 'home_page.dart';
-export 'discover_page.dart';
-export 'add_news_page.dart';
-export 'bookmark_page.dart';
-export 'profile_page.dart';
-export 'nav_page.dart';
-export 'splash_screen.dart';
