0d01e529101884f445d2ce0df03d8a57b33a821b
Ali Alirahimi <alialirahimi642@gmail.com>
2022-10-17 17:44:17 +0330

    implements all logics of game in main.dart
    start game, move snake, generate food , check game over and end game.

--- snake_game/lib/main.dart
+++ snake_game/lib/main.dart
@@ -2,156 +2,233 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: snakeGame(),
+      home: SnakeGame(),
     );
   }
 }
 
-class snakeGame extends StatefulWidget {
-  const snakeGame({super.key});
+class SnakeGame extends StatefulWidget {
+  const SnakeGame({super.key});
 
   @override
-  State<snakeGame> createState() => _snakeGameState();
+  State<SnakeGame> createState() => _SnakeGameState();
 }
 
-class _snakeGameState extends State<snakeGame> {
-  var snakeDirection = 'right';
-
-  final int itemPerRow = 25;
-  final int itemPerCol = 40;
-  final randomGenerator = Random();
-  List<List<int>> snakeCoor = [
-    [0, 1],
-    [0, 0]
+class _SnakeGameState extends State<SnakeGame> {
+  final int _itemsPerRow = 25;
+  final int _itemsPerCol = 40;
+  final _randomGen = Random();
+  var _snakeCoors = [
+    [10, 10],
+    [10, 9]
   ];
-  var foodCoor = [0, 2];
-
-  var isPlaying = false;
+  var _foodCoor = [0, 2];
+  var snakeDirection = 'up';
+  var _isGamePlaying = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onVerticalDragUpdate: (details) {
-                  if (snakeDirection != 'up' && details.delta.dy > 0) {
-                    snakeDirection = 'down';
-                  } else if (snakeDirection != 'down' && details.delta.dy < 0) {
-                    snakeDirection = 'up';
-                  }
-                },
-                onHorizontalDragUpdate: (details) {
-                  if (snakeDirection != 'left' && details.delta.dx > 0) {
-                    snakeDirection = 'right';
-                  } else if (snakeDirection != 'right' &&
-                      details.delta.dx < 0) {
-                    snakeDirection = 'left';
-                  }
-                },
-                child: AspectRatio(
-                  aspectRatio: itemPerRow / (itemPerCol + 6),
-                  child: GridView.builder(
-                      physics: const NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: itemPerRow,
-                      ),
-                      itemCount: itemPerRow * itemPerCol,
-                      itemBuilder: (BuildContext context, int index) {
-                        Color? color;
-                        var x = index % itemPerRow;
-                        var y = (index / itemPerRow).floor();
-
-                        if (snakeCoor.first[0] == x &&
-                            snakeCoor.first[1] == y) {
-                          color = Colors.green;
-                        } else if (snakeCoor[1][0] == x &&
-                            snakeCoor[1][1] == y) {
-                          color = Colors.green[200];
-                        } else if (foodCoor[0] == x && foodCoor[1] == y) {
-                          color = Colors.red;
-                        } else {
-                          color = Colors.grey[800];
-                        }
-                        return Container(
-                          margin: const EdgeInsets.all(1),
-                          decoration: BoxDecoration(
-                            color: color,
-                            shape: BoxShape.circle,
-                          ),
-                        );
-                      }),
-                ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (snakeDirection != 'up' && details.delta.dy > 0) {
+                  snakeDirection = 'down';
+                } else if (snakeDirection != 'down' && details.delta.dy < 0) {
+                  snakeDirection = 'up';
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (snakeDirection != 'left' && details.delta.dx > 0) {
+                  snakeDirection = 'right';
+                } else if (snakeDirection != 'right' && details.delta.dx < 0) {
+                  snakeDirection = 'left';
+                }
+              },
+              child: AspectRatio(
+                aspectRatio: _itemsPerRow / (_itemsPerCol + 5),
+                child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: _itemsPerRow,
+                    ),
+                    itemCount: _itemsPerRow * _itemsPerCol,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        margin: const EdgeInsets.all(1),
+                        decoration: BoxDecoration(
+                          color: _getItemColor(index),
+                          shape: BoxShape.circle,
+                        ),
+                      );
+                    }),
               ),
             ),
-            Padding(
-                padding: EdgeInsets.only(bottom: 20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: isPlaying ? Colors.red : Colors.blue,
-                        ),
-                        child: Text(
-                          isPlaying ? 'End' : 'Start',
-                          style: const TextStyle(
-                              color: Colors.white, fontSize: 20),
-                        ),
-                        onPressed: () {
-                          if (isPlaying) {
-                            isPlaying = false;
-                          } else {
-                            startGame();
-                          }
-                        }),
-                    Text(
-                      'Score: ${snakeCoor.length - 2}',
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          _isGamePlaying ? Colors.red : Colors.green,
+                    ),
+                    child: Text(
+                      _isGamePlaying ? 'End' : 'Start',
                       style: const TextStyle(color: Colors.white, fontSize: 20),
                     ),
-                  ],
-                )),
-          ],
-        ),
+                    onPressed: () {
+                      if (_isGamePlaying) {
+                        _isGamePlaying = false;
+                      } else {
+                        _startGame();
+                      }
+                    }),
+                Text(
+                  'Score: ${_snakeCoors.length - 2}',
+                  style: const TextStyle(color: Colors.white, fontSize: 20),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 
-  void startGame() {
-    const duration = Duration(milliseconds: 300);
-    isPlaying = true;
+  Color? _getItemColor(int index) {
+    var x = index % _itemsPerRow;
+    var y = (index / _itemsPerRow).floor();
+
+    bool isSnakeBody = false;
+    for (var coordinate in _snakeCoors) {
+      if (coordinate[0] == x && coordinate[1] == y) {
+        isSnakeBody = true;
+        break;
+      }
+    }
+
+    if (_snakeCoors.first[0] == x && _snakeCoors.first[1] == y) {
+      return Colors.blue;
+    } else if (isSnakeBody) {
+      return Colors.blue[200];
+    } else if (_foodCoor[0] == x && _foodCoor[1] == y) {
+      return Colors.red;
+    } else {
+      return const Color.fromARGB(255, 81, 80, 80);
+    }
+  }
+
+  void _startGame() {
+    _isGamePlaying = true;
+    const duration = Duration(milliseconds: 200);
+    _snakeCoors = [
+      [(_itemsPerRow / 2).floor(), (_itemsPerCol / 2).floor()]
+    ];
+    _snakeCoors.add([_snakeCoors.first[0], _snakeCoors.first[1] + 1]);
+    _generateFood();
     Timer.periodic(duration, (Timer timer) {
-      movesnake();
+      _moveSnake();
+      if (_checkGameOver()) {
+        timer.cancel();
+        _endGame();
+      }
     });
   }
 
-  void movesnake() {
+  void _moveSnake() {
     setState(() {
       switch (snakeDirection) {
         case 'up':
+          _snakeCoors
+              .insert(0, [_snakeCoors.first[0], _snakeCoors.first[1] - 1]);
           break;
 
         case 'down':
+          _snakeCoors
+              .insert(0, [_snakeCoors.first[0], _snakeCoors.first[1] + 1]);
           break;
 
         case 'left':
+          _snakeCoors
+              .insert(0, [_snakeCoors.first[0] - 1, _snakeCoors.first[1]]);
           break;
 
         case 'right':
+          _snakeCoors
+              .insert(0, [_snakeCoors.first[0] + 1, _snakeCoors.first[1]]);
           break;
       }
+
+      if (_snakeCoors.first[0] != _foodCoor[0] ||
+          _snakeCoors.first[1] != _foodCoor[1]) {
+        _snakeCoors.removeLast();
+      } else {
+        _generateFood();
+      }
     });
+  }
+
+  void _generateFood() {
+    _foodCoor = [
+      _randomGen.nextInt(_itemsPerRow),
+      _randomGen.nextInt(_itemsPerCol)
+    ];
+  }
+
+  bool _checkGameOver() {
+    if (!_isGamePlaying ||
+        _snakeCoors.first[1] < 0 ||
+        _snakeCoors.first[1] >= _itemsPerCol ||
+        _snakeCoors.first[0] < 0 ||
+        _snakeCoors.first[0] > _itemsPerRow) {
+      return true;
+    }
+
+    for (var i = 1; i < _snakeCoors.length; ++i) {
+      if (_snakeCoors[i][0] == _snakeCoors.first[0] &&
+          _snakeCoors[i][1] == _snakeCoors.first[1]) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  void _endGame() {
+    _isGamePlaying = false;
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('Game Over'),
+            content: Text(
+              'Score: ${_snakeCoors.length - 2}',
+              style: const TextStyle(fontSize: 20),
+            ),
+            actions: [
+              ElevatedButton(
+                child: const Text('Close'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
   }
 }
