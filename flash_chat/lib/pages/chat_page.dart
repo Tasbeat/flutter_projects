2069a0a7eb4c15dd0433929516cbeadfa92a56be
Ali Alirahimi <alialirahimi642@gmail.com>
2022-10-31 18:04:35 +0330

    make better UI with List View and message bubble

--- flash_chat/lib/pages/chat_page.dart
+++ flash_chat/lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -17,6 +19,7 @@ class _ChatScreenState extends State<ChatScreen> {
   final firestore = FirebaseFirestore.instance;
   String? messageText;
   User? loggedInUser;
+  var messageTextController = TextEditingController();
   @override
   void initState() {
     super.initState();
@@ -78,21 +81,32 @@ class _ChatScreenState extends State<ChatScreen> {
                 if (!snapshot.hasData) {
                   return const Center(
                     child: CircularProgressIndicator(
-                        backgroundColor: Colors.lightBlueAccent),
+                      backgroundColor: Colors.lightBlueAccent,
+                    ),
                   );
                 }
-                final messages = snapshot.data!.docs;
-                List<Text> messageWidgets = [];
+                final messages = snapshot.data!.docs.reversed;
+                List<MessageBubble> messageBubbles = [];
                 for (var message in messages) {
                   final messageText = message.get('text');
                   final messageSender = message.get('sender');
 
-                  final messageWidget =
-                      Text('$messageText from $messageSender');
-                  messageWidgets.add(messageWidget);
+                  messageBubbles.add(
+                    MessageBubble(
+                      messageText: messageText,
+                      messageSender: messageSender,
+                      isCurrentUser: messageSender == loggedInUser,
+                    ),
+                  );
                 }
-                return Column(
-                  children: messageWidgets,
+                return Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(bottom: 15.0),
+                    child: ListView(
+                      reverse: true,
+                      children: messageBubbles,
+                    ),
+                  ),
                 );
               },
             ),
@@ -107,6 +121,7 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: [
                   Expanded(
                     child: TextField(
+                      controller: messageTextController,
                       onChanged: (value) {
                         messageText = value;
                       },
@@ -122,6 +137,7 @@ class _ChatScreenState extends State<ChatScreen> {
                     onPressed: () {
                       firestore.collection('messages').add(
                           {'text': messageText, 'sender': loggedInUser!.email});
+                      messageTextController.clear();
                     },
                     child: const Text(
                       'Send',
@@ -138,6 +154,60 @@ class _ChatScreenState extends State<ChatScreen> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  final String messageText;
+  final String messageSender;
+  final bool isCurrentUser;
+  const MessageBubble(
+      {super.key,
+      required this.messageText,
+      required this.messageSender,
+      required this.isCurrentUser});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+          child: Text(
+            messageSender,
+            style: const TextStyle(color: Colors.black38, fontSize: 12),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+          child: Material(
+            color: isCurrentUser ? Colors.amber : Colors.blue,
+            elevation: 10.0,
+            borderRadius: isCurrentUser
+                ? const BorderRadius.only(
+                    topRight: Radius.circular(15.0),
+                    bottomLeft: Radius.circular(15.0),
+                    bottomRight: Radius.circular(15.0),
+                  )
+                : const BorderRadius.only(
+                    topLeft: Radius.circular(15.0),
+                    bottomLeft: Radius.circular(15.0),
+                    bottomRight: Radius.circular(15.0),
+                  ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Text(
+                messageText,
+                style: const TextStyle(
+                  fontSize: 18.0,
+                ),
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
