c05afaad2b3de4c00f7eccd2e488fa780c6bcc91
Ali Alirahimi <alialirahimi642@gmail.com>
2022-11-28 16:05:04 +0330

    implement sample category list and banner slider

--- ecommerce_app/lib/constants/colors.dart
+++ ecommerce_app/lib/constants/colors.dart
@@ -6,5 +6,5 @@ class CustomColors {
   static const Color blue = Color(0xff3B5EDF);
   static const Color red = Color(0xffD02026);
   static const Color green = Color(0xff1DB68B);
-  static const Color gery = Color(0xff858585);
+  static const Color grey = Color(0xff858585);
 }
--- ecommerce_app/lib/ui/pages/home_page.dart
+++ ecommerce_app/lib/ui/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/constants/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
@@ -13,42 +14,25 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: CustomColors.backgroundScreenColor,
       body: SafeArea(
-        child: Stack(
-          alignment: AlignmentDirectional.bottomCenter,
-          children: [
-            SizedBox(
-              height: 200.0,
-              child: Directionality(
-                textDirection: TextDirection.rtl,
-                child: PageView.builder(
-                  itemCount: 3,
-                  controller: pageViewController,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(left: 12.0, right: 12.0),
-                      child: Container(
-                        height: 200.0,
-                        color: Colors.blue,
-                      ),
-                    );
-                  },
-                ),
-              ),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: SliderBanner(pageViewController: pageViewController),
             ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
+            SliverToBoxAdapter(
               child: Directionality(
                 textDirection: TextDirection.rtl,
-                child: SmoothPageIndicator(
-                  controller: pageViewController,
-                  count: 3,
-                  effect: const ExpandingDotsEffect(
-                    expansionFactor: 4,
-                    dotHeight: 10,
-                    dotWidth: 10,
-                    dotColor: Colors.black,
-                    activeDotColor: Colors.red,
+                child: Container(
+                  color: Colors.transparent,
+                  height: 120,
+                  child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return const CategoryItem();
+                    },
                   ),
                 ),
               ),
@@ -56,6 +40,101 @@ class _HomepageState extends State<Homepage> {
           ],
         ),
       ),
+    );
+  }
+}
+
+class CategoryItem extends StatelessWidget {
+  const CategoryItem({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 8.0),
+          child: Container(
+            width: 56.0,
+            height: 56.0,
+            decoration: ShapeDecoration(
+              shadows: const [
+                BoxShadow(
+                  color: CustomColors.red,
+                  blurRadius: 30,
+                  spreadRadius: -6,
+                  offset: Offset(0.0, 10.0),
+                )
+              ],
+              color: CustomColors.red,
+              shape: ContinuousRectangleBorder(
+                borderRadius: BorderRadius.circular(40.0),
+              ),
+            ),
+            child: const Icon(
+              Icons.mouse,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        Text('data')
+      ],
+    );
+  }
+}
+
+class SliderBanner extends StatelessWidget {
+  const SliderBanner({
+    Key? key,
+    required this.pageViewController,
+  }) : super(key: key);
+
+  final PageController pageViewController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.bottomCenter,
+      children: [
+        SizedBox(
+          height: 200.0,
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: PageView.builder(
+              itemCount: 3,
+              controller: pageViewController,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 12.0, right: 12.0),
+                  child: Container(
+                    height: 200.0,
+                    color: Colors.blue,
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(bottom: 8.0),
+          child: Directionality(
+            textDirection: TextDirection.rtl,
+            child: SmoothPageIndicator(
+              controller: pageViewController,
+              count: 3,
+              effect: const ExpandingDotsEffect(
+                expansionFactor: 4,
+                dotHeight: 10,
+                dotWidth: 10,
+                dotColor: Colors.black,
+                activeDotColor: Colors.red,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
