1e2a900f8bec7eb7a084d89d63ca577327b5d1ca
Ali Alirahimi <alialirahimi642@gmail.com>
2022-12-02 17:41:19 +0330

    implement scan and connect to device operations

--- bluetooth_sample/test/widget_test.dart
+++ bluetooth_sample/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:bluetooth_sample/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const FlutterBlueApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
